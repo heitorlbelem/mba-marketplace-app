b639fd043f6a7758da00cde232d43b04bccf5648
Heitor Belém <heitorlbelem98@gmail.com>
2024-10-11 17:55:33 -0300

    Finish sign-in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -8,8 +8,8 @@ export function SignIn() {
   return (
     <>
       <Helmet title="Entrar" />
-      <div className="h-screen w-screen bg-background">
-        <div className="flex mx-auto max-w-[1380px] gap-6">
+      <div className="h-screen w-screen bg-background flex align-center justify-center">
+        <div className="grid grid-cols-[755px_563px]">
           <div className="flex flex-col">
             <div className="flex gap-5 items-center p-10">
               <img src={logo} alt="" />
@@ -22,59 +22,72 @@ export function SignIn() {
             </div>
             <img src={backgroundAuth} alt="" className="w-[755px] h-[496px]" />
           </div>
-          <form className="mt-6 bg-white rounded-[32px] py-16 px-20">
-            <header className="flex flex-col gap-2 mb-12">
-              <h1 className="font-title font-bold text-2xl">
-                Acesse sua conta
-              </h1>
-              <p className="font-base font-normal text-sm text-gray-300">
-                Informe seu e-mail e senha para entrar
+          <div className="my-6 bg-white rounded-[32px] py-16 px-20 flex flex-col justify-between">
+            <form className="flex flex-col gap-12">
+              <header className="flex flex-col gap-2">
+                <h1 className="font-title font-bold text-2xl">
+                  Acesse sua conta
+                </h1>
+                <p className="font-base font-normal text-sm text-gray-300">
+                  Informe seu e-mail e senha para entrar
+                </p>
+              </header>
+
+              <fieldset className="flex flex-col gap-5">
+                <div className="flex flex-col gap-1">
+                  <label
+                    className="text-xs font-base text-gray-300 uppercase"
+                    htmlFor="email"
+                  >
+                    e-mail
+                  </label>
+                  <div className="border-b-2 py-2 focus-within:border-b-orange-base flex gap-2 items-center">
+                    <MailIcon className="w-6 h-6 text-gray-200" />
+                    <input
+                      type="email"
+                      id="email"
+                      className="outline-none font-base font-normal text-base text-gray-200 flex-1"
+                      placeholder="Seu e-mail cadastrado"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex flex-col gap-1">
+                  <label
+                    className="text-xs font-base text-gray-300 uppercase"
+                    htmlFor="password"
+                  >
+                    senha
+                  </label>
+                  <div className="border-b-2 py-2 focus-within:border-b-orange-base flex gap-2 items-center">
+                    <KeyRoundIcon className="w-6 h-6 text-gray-200" />
+                    <input
+                      type="password"
+                      id="password"
+                      className="outline-none font-base font-normal text-base text-gray-200 flex-1"
+                      placeholder="Sua senha de acesso"
+                    />
+                  </div>
+                </div>
+              </fieldset>
+
+              <button className="bg-orange-base text-white p-5 flex items-center justify-between w-full rounded-xl hover:bg-orange-base/95 transition-all">
+                Acessar
+                <ArrowRightIcon className="text-white w-6 h-6" />
+              </button>
+            </form>
+
+            <div className="flex flex-col gap-5">
+              <p className="font-base font-normal text-base text-gray-300">
+                Ainda não tem uma conta?
               </p>
-            </header>
 
-            <fieldset className="flex flex-col gap-5">
-              <div className="flex flex-col gap-1">
-                <label
-                  className="text-xs font-base text-gray-300 uppercase"
-                  htmlFor="email"
-                >
-                  e-mail
-                </label>
-                <div className="border-b-2 py-2 focus-within:border-b-orange-base flex gap-2 items-center">
-                  <MailIcon className="w-6 h-6 text-gray-200" />
-                  <input
-                    type="email"
-                    id="email"
-                    className="outline-none font-base font-normal text-base text-gray-200 flex-1"
-                    placeholder="Seu e-mail cadastrado"
-                  />
-                </div>
-              </div>
-
-              <div className="flex flex-col gap-1">
-                <label
-                  className="text-xs font-base text-gray-300 uppercase"
-                  htmlFor="password"
-                >
-                  senha
-                </label>
-                <div className="border-b-2 py-2 focus-within:border-b-orange-base flex gap-2 items-center">
-                  <KeyRoundIcon className="w-6 h-6 text-gray-200" />
-                  <input
-                    type="password"
-                    id="password"
-                    className="outline-none font-base font-normal text-base text-gray-200 flex-1"
-                    placeholder="Sua senha de acesso"
-                  />
-                </div>
-              </div>
-            </fieldset>
-
-            <button className="bg-orange-base text-white p-5 flex items-center justify-between w-full mt-12 rounded-xl hover:bg-orange-base/95 transition-all">
-              Acessar
-              <ArrowRightIcon className="text-white w-6 h-6" />
-            </button>
-          </form>
+              <button className="bg-white text-orange-base border-orange-base border-2 p-5 flex items-center justify-between w-full rounded-xl">
+                Cadastrar
+                <ArrowRightIcon className="text-orange-base w-6 h-6" />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
