f9ed9b4175cdf8e431b68484c9d0a7e0b1abb1f0
Heitor Belém <heitorlbelem98@gmail.com>
2024-10-16 10:47:51 -0300

    Adding structure to be able to make requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { queryClient } from './lib/react-query'
 import { router } from './router'
 
 export function App() {
   return (
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </HelmetProvider>
   )
 }
