4465a7b10f0419c9d29cedd6944338d1dfc424a7
Heitor Belém <heitorlbelem98@gmail.com>
2024-10-16 11:20:50 -0300

    Finishes sign-in page functionality

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,13 +1,42 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { ArrowRightIcon, EyeIcon, KeyRoundIcon, MailIcon } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
+import { z } from 'zod'
 
+import { signIn } from '../../api/sign-in'
 import { AuthFormHeader } from '../../components/AuthFormHeader'
 import { InputContainer } from '../../components/InputContainer'
 import { InputWrapper } from '../../components/InputWrapper'
 
+const signInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type SignInFormType = z.infer<typeof signInSchema>
+
 export function SignIn() {
   const navigate = useNavigate()
+
+  const { register, handleSubmit } = useForm<SignInFormType>({
+    resolver: zodResolver(signInSchema),
+  })
+
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
+  async function handleSignIn(data: SignInFormType) {
+    try {
+      const { email, password } = data
+      await authenticate({ email, password })
+    } catch {
+      console.error('Credenciais inválidas.')
+    }
+  }
 
   return (
     <>
@@ -18,23 +47,35 @@ export function SignIn() {
           pageDescription="Informe seu e-mail e senha para entrar"
         />
 
-        <form className="flex flex-col gap-12">
+        <form
+          className="flex flex-col gap-12"
+          onSubmit={handleSubmit(handleSignIn)}
+        >
           <fieldset className="flex flex-col gap-5">
             <InputWrapper.Root>
-              <InputWrapper.Label>e-mail</InputWrapper.Label>
+              <InputWrapper.Label htmlFor="email">e-mail</InputWrapper.Label>
               <InputContainer.Root>
                 <InputContainer.Icon icon={MailIcon} />
-                <InputContainer.TextField placeholder="Seu e-mail cadastrado" />
+                <input
+                  className="flex-1 bg-transparent font-base text-base font-normal text-gray-200 outline-none"
+                  placeholder="Seu e-mail cadastrado"
+                  type="email"
+                  id="email"
+                  {...register('email')}
+                />
               </InputContainer.Root>
             </InputWrapper.Root>
 
             <InputWrapper.Root>
-              <InputWrapper.Label>Senha</InputWrapper.Label>
+              <InputWrapper.Label htmlFor="password">senha</InputWrapper.Label>
               <InputContainer.Root>
                 <InputContainer.Icon icon={KeyRoundIcon} />
-                <InputContainer.TextField
+                <input
+                  className="flex-1 bg-transparent font-base text-base font-normal text-gray-200 outline-none"
                   placeholder="Sua senha de acesso"
                   type="password"
+                  id="password"
+                  {...register('password')}
                 />
                 <InputContainer.Action icon={EyeIcon} />
               </InputContainer.Root>
