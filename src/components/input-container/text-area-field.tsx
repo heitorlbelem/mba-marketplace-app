2ce73b86ee11643b8e4c0e90a7345c9007124ab8
Heitor Belém <heitorlbelem98@gmail.com>
2024-10-18 01:51:51 -0300

    Convert text-area to foward ref to receive register from react hook forms

--- src/components/input-container/text-area-field.tsx
+++ src/components/input-container/text-area-field.tsx
@@ -1,12 +1,16 @@
-import { TextareaHTMLAttributes } from 'react'
+import { forwardRef, TextareaHTMLAttributes } from 'react'
 
-export function TextAreaField(
-  props: TextareaHTMLAttributes<HTMLTextAreaElement>,
-) {
+export const TextAreaField = forwardRef<
+  HTMLTextAreaElement,
+  TextareaHTMLAttributes<HTMLTextAreaElement>
+>((props, ref) => {
   return (
     <textarea
+      ref={ref}
       className="min-h-32 flex-1 bg-transparent font-base text-base font-normal text-gray-200 outline-none"
       {...props}
     />
   )
-}
+})
+
+TextAreaField.displayName = 'TextAreaField'
